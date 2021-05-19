0e04308e80a142b64cefe60c79f45ef36fc31b71
haerulwardy <heydicreative@gmail.com>
2021-05-19 19:44:51 +0700

    create responsive design

--- js/main.js
+++ js/main.js
@@ -11,12 +11,22 @@ const swiper = new Swiper('.swiper-container', {
       prevEl: '.swiper-button-prev',
     },
     slidesPerView: 3,
-        spaceBetween: 30,
-        freeMode: true,
-        pagination: {
-          el: ".swiper-pagination",
-          clickable: true,
-        },
+    spaceBetween: 30,
+    freeMode: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      720: {
+        slidesPerView: 1,
+        spaceBetweenSlides: 30
+      },
+      540: {
+        slidesPerView: 1,
+        spaceBetweenSlides: 30
+      }
+    }
   });
 // Now you can use all slider methods like
 swiper.slideNext();
