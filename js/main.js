30f88c58d31bd7c81e59f2983c694f95e656ea6e
haerulwardy <heydicreative@gmail.com>
2021-05-19 19:02:20 +0700

    initialization

---
+++ js/main.js
@@ -0,0 +1,22 @@
+const swiper = new Swiper('.swiper-container', {
+    // Optional parametersS
+    loop: true,
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    slidesPerView: 3,
+        spaceBetween: 30,
+        freeMode: true,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+  });
+// Now you can use all slider methods like
+swiper.slideNext();
