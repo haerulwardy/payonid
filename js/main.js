49b74f40a894b7f77f6ca4743214ed550c35c2fc
haerulwardy <heydicreative@gmail.com>
2021-05-20 09:39:19 +0700

    add responsive menu

--- js/main.js
+++ js/main.js
@@ -19,7 +19,7 @@ const swiper = new Swiper('.swiper-container', {
     },
     breakpoints: {
       1140: {
-        slidesPerView: 3,
+        slidesPerView: 4,
         spaceBetweenSlides: 30
       },
       720: {
@@ -42,10 +42,112 @@ swiper.slideNext();
 // show menu function
 function showMenu() {
     // change icon
-    changeIcon();
+    addChangeIcon();
+    // drop menu animation
+    addDropMenuAnimation();
 }
+function closeMenu() {
+  // change icon
+  closeChangeIcon();
+  // drop menu animation
+  closeDropMenuAnimation();
+}
+
 // change icon function
-function changeIcon() {
+function addChangeIcon() {
+  const hamburgerMenu = document.querySelector('.header-hamburger');
+  const closeMenu = document.querySelector('.header-close');
+  hamburgerMenu.classList.add('hide');
+  closeMenu.classList.add('show');
+}
+
+// function dropMenuAnimation
+function addDropMenuAnimation() {
   const responsiveMenu = document.querySelector('.responsive-menu');
-  responsiveMenu.classList.toggle('active')
+  responsiveMenu.classList.add('active');
+
+  responsiveMenus('add');
+}
+
+function responsiveMenus(action) {
+  const responsiveMenus = document.querySelector('.responsive-menus');
+  if(action === 'add') {
+    responsiveMenus.classList.add('active')
+  } else if(action === 'remove') {
+    responsiveMenus.classList.remove('active')
+  }
+}
+
+// change icon function
+function closeChangeIcon() {
+  const hamburgerMenu = document.querySelector('.header-hamburger');
+  const closeMenu = document.querySelector('.header-close');
+  hamburgerMenu.classList.remove('hide');
+  closeMenu.classList.remove('show');
+}
+
+// function dropMenuAnimation
+function closeDropMenuAnimation() {
+  const responsiveMenu = document.querySelector('.responsive-menu');
+  responsiveMenu.classList.remove('active');
+
+  // responsive menus
+  responsiveMenus('remove');
+}
+
+// window on scroll
+window.addEventListener('scroll', function() {
+  // console.log(window.scrollY = 100);
+  // header
+  if( window.scrollY <= 0 ) {
+    changeBgHeader('remove');
+    headerMenus('remove');
+
+    // show logo
+    showLogo();
+    // btn
+    showBtnDownload();
+  } else if ( window.scrollY > 0 ) {
+    changeBgHeader('add');
+    headerMenus('add');
+
+    // hide logo
+    hideLogo();
+    // btn
+    hideBtnDownload();
+  }
+})
+
+// function change bg header
+function changeBgHeader(action) {
+  const header = document.getElementById('header');
+  if(action === 'remove') {
+    header.classList.remove('scroll');
+  } else if(action === 'add') {
+    header.classList.add('scroll');
+  }
+}
+
+// function header menus
+function headerMenus(action) {
+  const headerMenus = document.querySelectorAll('.header-menus a');
+  if(action === 'remove') {
+    headerMenus.forEach(menu => {
+      menu.classList.remove('scroll');
+    })
+  } else if(action === 'add') {
+    headerMenus.forEach(menu => {
+      menu.classList.add('scroll');
+    })
+  }
+}
+// function header menus
+function hideLogo() {
+  document.getElementById('main-logo').classList.add('hide-logo');
+  document.getElementById('secondary-logo').classList.remove('hide-logo');
+}
+// function header menus
+function showLogo() {
+  document.getElementById('main-logo').classList.remove('hide-logo');
+  document.getElementById('secondary-logo').classList.add('hide-logo');
 }
