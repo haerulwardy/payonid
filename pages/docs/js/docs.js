153d55532e8b72bedd4a8fa4b5b22e55e96ee25f
haerulwardy <heydicreative@gmail.com>
2021-06-01 10:50:48 +0700

    create rhe doc pages

--- pages/docs/js/docs.js
+++ pages/docs/js/docs.js
@@ -1,1 +1,15 @@
-alert('ok');
+// menu actiive
+function menuActive() {
+    const sidebarMenus = document.querySelectorAll('.sidebar-menu a');
+    sidebarMenus.forEach( menu => {
+        menu.addEventListener('click', function(e) {
+            e.preventDefault();
+            sidebarMenus.forEach(m => {
+                m.classList.remove('active');
+            })
+            menu.classList.add('active');
+        })
+    })
+}
+// run function
+menuActive();
