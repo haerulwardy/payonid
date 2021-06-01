c27459fc79391dbcf8d532469825d128f0a479ae
haerulwardy <heydicreative@gmail.com>
2021-06-02 05:55:34 +0700

    responsive menus

--- pages/docs/js/docs.js
+++ pages/docs/js/docs.js
@@ -2,8 +2,7 @@
 function menuActive() {
     const sidebarMenus = document.querySelectorAll('.sidebar-menu a');
     sidebarMenus.forEach( menu => {
-        menu.addEventListener('click', function(e) {
-            e.preventDefault();
+        menu.addEventListener('click', function() {
             sidebarMenus.forEach(m => {
                 m.classList.remove('active');
             })
@@ -11,5 +10,58 @@ function menuActive() {
         })
     })
 }
+
+// res menu
+function resMenu() {
+    const resMenus = document.querySelectorAll('.res-menu');
+    const resContent = document.querySelectorAll('.res-content');
+    resMenus.forEach(menu => {
+        menu.addEventListener('click', function() {
+            resContent.forEach(content => {
+                content.classList.remove('active')
+            })
+            showMenus(this);
+        })
+    })
+
+    function showMenus(content) {
+        if(content.className.includes('res-login')) {
+            const conLogin = document.querySelector('.content-login');
+            conLogin.classList.toggle('active');
+        } else if(content.className.includes('res-rincian')) {
+            const conRincian = document.querySelector('.content-rincian');
+            conRincian.classList.toggle('active');
+        } else if(content.className.includes('res-produk')) {
+            const conProduk = document.querySelector('.content-produk');
+            conProduk.classList.toggle('active');
+        } else if(content.className.includes('res-pembelian')) {
+            const conPembelian = document.querySelector('.content-pembelian');
+            conPembelian.classList.toggle('active');
+        } else if(content.className.includes('res-transaksi')) {
+            const conTransaksi = document.querySelector('.content-transaksi');
+            conTransaksi.classList.toggle('active');
+        }
+    }
+}
+
+// conten menu
+function resContentMenu() {
+    const resContentMenus = document.querySelectorAll('.res-content-menu a');
+    resContentMenus.forEach(menu => {
+        menu.addEventListener('click', function(e) {
+            e.preventDefault();
+            resContentMenus.forEach(m => {
+                m.classList.remove('active');
+            })
+
+            this.classList.add('active');
+        })
+    })
+}
+
+
+
 // run function
 menuActive();
+resMenu();
+resContentMenu();
